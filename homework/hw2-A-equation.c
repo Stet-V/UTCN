e9f7b6afc1a30526e553c2b4e800d249e0f6cfe2
Asty <stetvalex@proton.me>
2024-10-16 15:01:36 +0300

    fix

--- homework/hw2-A-equation.c
+++ homework/hw2-A-equation.c
@@ -3,11 +3,10 @@
 int main() {
     float a, b, c;
     char x;
-    scanf("%f %c %f = %f", &a, &x, &b, &c);
-    if (c)
-        if ((x == '+' && a + b == c) || (x == '*' && a * b == c))
-            printf("true\n");
-         else printf("false\n");
+    int r = scanf("%f %c %f = %f", &a, &x, &b, &c);
+    if (r == 4)
+        if ((x == '+' && a + b == c) || (x == '*' && a * b == c)) printf("true\n");
+        else printf("false\n");
     else printf("no operator\n");
     return 0;
 }
