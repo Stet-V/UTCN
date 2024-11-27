2c3a66322f05bb278ca4021d9d76007bb0b5dffd
Asty <stetvalex@proton.me>
2024-11-27 23:36:13 +0200

    fix

--- homework/hw8/src/main.c
+++ homework/hw8/src/main.c
@@ -3,20 +3,21 @@
 #include "poe.h"
 
 int main() {
+    // int H = 3, W = 5;
     // char a[3][5] = {
     //     {'.', '.', '.', 'm', '.'},
     //     {'.', 'A', '.', '.', '1'},
     //     {'.', 'B', '.', '0', '.'}
     // };
     // print_map(H, W, a);
-    // char *s = calloc(H * W * 10, sizeof(char));
+    // char *s = calloc(H * W * 6, sizeof(char));
     // encode(H, W, a, s);
     // printf("%s\n", s);
     int H = 3, W = 5;
     char a[3][5] = {
         {'.', 'm', '.', 'm', '.'},
         {'.', '.', 'A', '.', '0'},
-        {'B', '.', '0', '1', '1'}
+        {'B', '.', '1', '2', '3'}
     };
     print_map(H, W, a);
     char s[] = "m A4 A B2 o1 B5 B C2 o0 C4";
