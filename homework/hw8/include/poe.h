7b965733589c737bde7fd000709da58f285ef6b3
Asty <stetvalex@proton.me>
2024-11-29 17:37:37 +0200

    fix

--- homework/hw8/include/poe.h
+++ homework/hw8/include/poe.h
@@ -6,6 +6,7 @@ char row_label(int i);
 void print_map(int H, int W, char a[H][W]);
 int find_in_map(int H, int W, char a[H][W], char x, char rows[], int cols[]);
 void encode(int H, int W, char a[H][W], char* s);
+int row_label_index(char c);
 void decode(int H, int W, char a[H][W], char* s);
 
 #endif // POE_H
